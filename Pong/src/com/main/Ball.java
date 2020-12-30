dadb13122de62213d10f7c855c819c416eebf7f5
kgrigos <konstantinos.grigos@gmail.com>
2020-12-30 21:57:35 +0200

    first commit

---
+++ Pong/src/com/main/Ball.java
@@ -0,0 +1,78 @@
+package com.main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Ball {
+	public static final int SIZE=16;
+	
+	private int x,y;
+	private int xVel,yVel;//1 or -1
+	private int speed=9;
+	
+	public Ball() {
+		reset();
+		
+	}
+
+	private void reset() {
+		// initial position
+		
+		x=Game.WIDTH/2-SIZE/2;
+		y=Game.HEIGHT/2-SIZE/2;
+		
+		//initial velocity
+		
+		xVel = Game.sign(Math.random() * 2.0 - 1);
+		yVel = Game.sign(Math.random() * 2.0 - 1);
+
+		
+	}
+	
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void changeYDir() {
+		
+		yVel*=-1;
+	}
+	
+public void changeXDir() {
+		
+		xVel*=-1;
+	}
+
+public void draw(Graphics g) {
+	g.setColor(Color.white);
+	g.fillRect(x, y, SIZE, SIZE);
+}
+
+public void update(Paddle p1, Paddle p2) {
+	//update movement
+	x +=xVel*speed;
+	y+=yVel*speed;
+	
+	//collision
+	if(y+SIZE>=Game.HEIGHT || y<=0)
+		changeYDir();
+		
+	//with walls
+	if(x+SIZE>=Game.WIDTH) {
+		p1.addPoint();
+		reset();
+	}
+	if(x<=0) {
+		p2.addPoint();
+		reset();
+	}
+	
+	
+	
+}
+
+}
